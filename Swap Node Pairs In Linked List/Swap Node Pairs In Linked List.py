906b9639fed0efb7335e6d7e6ddf135f9ae949a1
Iryna Denysova <denysova.pn@ucu.edu.ua>
2024-04-03 16:29:07 +0300

    solved "Swap Node Pairs In Linked List"

---
+++ Swap Node Pairs In Linked List/Swap Node Pairs In Linked List.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def swap_pairs(head):
+    if not head or not head.next:
+        return head
+    new_head = head.next
+    while head and head.next:
+        first = head
+        second = head.next
+        first.next = second.next
+        second.next = first
+        if first.next and first.next.next:
+            first.next = first.next.next
+        head = first.next
+    return new_head
