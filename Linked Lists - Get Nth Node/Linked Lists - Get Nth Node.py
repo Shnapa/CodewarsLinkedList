4eaaa2b346c435961f1f0fed4732ecdda047ccc6
Iryna Denysova <denysova.pn@ucu.edu.ua>
2024-04-03 14:35:49 +0300

    made photos of passed tests bigger

--- Linked Lists - Get Nth Node/Linked Lists - Get Nth Node.py
+++ Linked Lists - Get Nth Node/Linked Lists - Get Nth Node.py
@@ -5,14 +5,14 @@ class Node(object):
         self.next = next
     
         
-    def get_nth(self, node, index):
-        count = 0
-        if index < 0 or not node:
-            raise ValueError
-        while node:
-            if count == index:
-                return node
-            count += 1
-            node = node.next
-
+def get_nth(node, index):
+    count = 0
+    if index < 0 or not node:
         raise ValueError
+    while node:
+        if count == index:
+            return node
+        count += 1
+        node = node.next
+
+    raise ValueError
